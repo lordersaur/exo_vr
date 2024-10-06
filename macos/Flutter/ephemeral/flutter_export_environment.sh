830025aa5e168a900703b46fb768df518731d89c
lordersaur <daxel152@gmail.com>
2024-10-06 17:44:12 -0300

    cambios de xcode

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/danielbrizuela/BrosCo-Dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/danielbrizuela/Documents/NASA /exo_vr"
+export "FLUTTER_ROOT=/Users/daxel/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/daxel/Documents/spaceapps/exo_vr"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
