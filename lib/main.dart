9123cad5986419f3997e4d644954572512a09750
Daniel Brizuela <carlosbrizuelamereles@gmail.com>
2024-10-06 15:18:06 -0300

    mini front added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:exo_vr/pages/planet_android_page.dart';
+import 'package:exo_vr/pages/home/screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const PlanetAndroidPage(),
+      home: const HomeScreen(),
     );
   }
 }
---
+++ lib/pages/home/screen.dart
@@ -0,0 +1,67 @@
+import 'package:exo_vr/pages/solar/solar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.deepPurple, Colors.black],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                left: 50,
+                right: 50,
+                child: Image.asset(
+                  'assets/images/moon.png',
+                  height: 300,
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const Positioned(
+                bottom: 180,
+                left: 30,
+                right: 30,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Explore The Solar System.",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "With 360 Degree interactive Tour.",
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const MarsPage(),
+        ],
+      ),
+    );
+  }
+}
